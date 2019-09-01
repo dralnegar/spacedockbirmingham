a328144967a99d885a4b263cfcc00377da487ad4
Stefan Harvey <stefan.harvey@btopenworld.com>
2019-09-01 17:35:16 +0100

    Initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,23 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Spacedock Birmingham</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+        <!-- Fonts -->
+        <link href="/lcars/css/lcars.min.css" rel="stylesheet">
+
         <!-- Styles -->
         <style>
+
+            html, body { background: black }
+            p, h1, h2, h3 
+            {
+                margin-top: 1em;
+            }
+            /*
             html, body {
                 background-color: #fff;
                 color: #636b6f;
@@ -19,6 +29,7 @@
                 height: 100vh;
                 margin: 0;
             }
+            */
 
             .full-height {
                 height: 100vh;
@@ -49,7 +60,7 @@
             }
 
             .links > a {
-                color: #636b6f;
+                color: #FFFFFF;
                 padding: 0 25px;
                 font-size: 13px;
                 font-weight: 600;
@@ -61,39 +72,115 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+
+            .lcarsElementDiv 
+            {   
+                border-left: .75rem solid #FC6;
+                border-right: .75rem solid #FC6;
+                padding:2%;
+        
+            }
+            
+            a:link, a:visited { color: white; }
+            a:hover, a:active {  color: hotpink; cursor: pointer; }
+
+            .centered
+            {
+                text-align:center;
+            }
+}
+
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <div class="lcars-app-container">
+            
+            <!-- HEADER ==================================================== -->
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+            <div id="header" class="lcars-row header">
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                <!-- ELBOW -->
+                <div class="lcars-elbow left-bottom lcars-tan-bg"></div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <!-- BAR WITH TITLE -->
+                <div class="lcars-bar horizontal">
+                    <div class="lcars-title right" style="color:white">
+                    @if (Route::has('login'))
+                        <div class="top-right links">
+                            @auth
+                                <a href="{{ url('/home') }}">Home</a>
+                            @else
+                                <a href="{{ route('login') }}">Login</a>
+
+                                @if (Route::has('register'))
+                                    <a href="{{ route('register') }}">Register</a>
+                                @endif
+                            @endauth
+                        </div> <!-- .top-right links -->
+                    @endif    
+                    </div> <!-- .lcars-title right --> 
+                </div> <!-- lcars-bar horizontal -->
+
+                <!-- ROUNDED EDGE DECORATED -->
+                <div class="lcars-bar horizontal right-end decorated"></div>
+            </div> <!-- .header -->
+      
+            <!-- SIDE MENU ================================================= -->
+            <div id="left-menu" class="lcars-column start-space lcars-u-1">
+                <!-- FILLER -->
+                <div class="lcars-bar lcars-u-1"></div>
+            </div> <!-- .left-menu -->
+
+            <div id="footer" class="lcars-row ">
+                <!-- ELBOW -->
+                <div class="lcars-elbow left-top lcars-tan-bg"></div>
+                <!-- BAR -->
+                <div class="lcars-bar horizontal both-divider bottom"></div>
+                <!-- ROUNDED EDGE -->
+                <div class="lcars-bar horizontal right-end left-divider bottom"></div>
             </div>
-        </div>
+
+            <!-- MAIN CONTAINER -->
+		<div id="container">
+			<!-- COLUMN LAYOUT -->
+			<div class="lcars-column lcars-u-12">
+                <div class="lcars-row">
+                    <div class="lcars-u-3">
+                        <div class="lcars-bracket top hollow"></div>
+                        <div class="lcarsElementDiv">
+                            <a href="https://www.facebook.com/spacedockbirmingham" target="blank">
+                                <img src="/img/spacedock_birmingham_logo.jpg" alt="Spacedock Birmingham" style="width:100%"/>        
+                            </a>
+                        </div>
+                    
+                        <div class="lcars-bracket bottom hollow centered">
+                            <a href="https://www.facebook.com/spacedockbirmingham" target="blank">Spacedock Birmingham</a>
+                        </div>
+                    </div>
+
+                    <div class="lcars-u-3">
+                        <div class="lcars-bracket top hollow"></div>
+                        <div class="lcarsElementDiv">
+                            <a href="https://www.facebook.com/svenof9" target="blank">
+                                <img src="/img/sven_in_uniform.jpg" alt="Sven in Uniform" style="width:100%"/>        
+                            </a>
+                        </div>
+                    
+                        <div class="lcars-bracket bottom hollow centered"><a href="https://www.facebook.com/svenof9" target="blank">Sven In Uniform</a></div>
+                    </div>
+                    
+                    <div class="lcars-u-3">
+                        <div class="lcars-bracket top hollow"></div>
+                        <div class="lcarsElementDiv">
+                            <a href="https://www.facebook.com/david.zabi" target="blank">    
+                                <img src="/img/david_in_uniform.jpg" alt="David in Uniform" style="width:100%"/>        
+                            </a>
+                        </div>
+                        
+                        <div class="lcars-bracket bottom hollow centered"><a href="https://www.facebook.com/david.zabi" target="blank">David In Uniform</a></div>
+                    </div>
+                </div>  <!-- .lcars-row -->
+            </div> <!-- #content -->
+        </div> <!-- .lcars--app-container -->
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
+Auth::routes();
+
+Route::get('/admin', 'HomeController@index')->name('home');
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+// Route::get('/home', 'HomeController@index')->name('home');
