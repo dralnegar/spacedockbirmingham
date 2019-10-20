b86e4646097da1e3fb891f42b3451acd30759016
Stefan Harvey <stefan.harvey@btopenworld.com>
2019-10-20 14:24:38 +0100

    fix(Website): Corrected the controller name in AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class AdminController extends Controller
 {
     /**
      * Create a new controller instance.
@@ -23,7 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        die('REACHED HERE');
         return view('home');
     }
 }
