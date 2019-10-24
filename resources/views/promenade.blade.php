ddd07c8749eef4829b920148537c165cd0cafa2e
Stefan Harvey <stefan.harvey@btopenworld.com>
2019-10-24 23:37:05 +0100

    refactor(Website): Added content for the Promenade page

--- resources/views/promenade.blade.php
+++ resources/views/promenade.blade.php
@@ -7,6 +7,15 @@
         <div class="lcars-bracket top hollow"></div>
         <div class="lcarsElementDiv lcars-text">
         PROMENADE        
+        <p>
+        Spacedock Birmingham merchandise including ‘zines and badges will be here to buy soon.
+        </p>
+        <p>
+        <a href="https://www.facebook.com/groups/SpacedockBirminghamPromenade" target="_blank">
+        <img src="/img/promenade_header.png" style="width:100%;margin-right:auto;margin-right:auto;"/>
+        </a>
+        The Birmingham Promenade Facebook Sales Group
+        </p>
         </div>
         <div class="lcars-bracket bottom hollow"></div>
     </div>
