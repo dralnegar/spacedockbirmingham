05652d26c89ee9f2cd0d8c211ad9d56ebce6a72f
Stefan Harvey <stefan.harvey@btopenworld.com>
2019-10-25 00:56:57 +0100

    refactor(Website): Other Links page content

--- resources/views/other.blade.php
+++ resources/views/other.blade.php
@@ -6,9 +6,51 @@
     <div class="lcars-column lcars-u-12">
         <div class="lcars-bracket top hollow"></div>
         <div class="lcarsElementDiv lcars-text">
-        OTHER LINKS     
+        <a href="https://www.facebook.com/groups/autobasebham/" target="_blank">
+        <img src="/img/autobase_birmingham_oilhouse.png" style="width:100%;margin-right:auto;margin-right:auto;"/>
+        </a>
+        <p>Autobase Birmingham Oilhouse Facebook Group</p>
+        </div>
+        <div class="lcars-bracket bottom hollow" style="margin-bottom:10px"></div>
+
+        <div class="lcars-bracket top hollow" style="border-color:#0098A4;"></div>
+        <div class="lcarsElementDiv lcars-text" style="border-color:#0098A4;">
+        <a href="https://www.facebook.com/groups/4MatClub/" target="_blank">
+        <img src="/img/geekology_4m@.png" style="width:100%;margin-right:auto;margin-right:auto;"/>
+        </a>
+        <p>Club 4M@ Facebook Group</p>
+        </div>
+        <div class="lcars-bracket bottom hollow" style="border-color:#0098A4;margin-bottom:10px"></div>
+
+        <div class="lcars-bracket top hollow"></div>
+        <div class="lcarsElementDiv lcars-text">
+        <a href="https://www.facebook.com/groups/geekologycom/" target="_blank">
+        <img src="/img/geekology_group.png" style="width:100%;margin-right:auto;margin-right:auto;"/>
+        </a>
+        <p>Geekology: The Group Facebook Group</p>
+        </div>
+        <div class="lcars-bracket bottom hollow" style="margin-bottom:10px"></div>
+
+        <div class="lcars-bracket top hollow" style="border-color:#0098A4;"></div>
+        <div class="lcarsElementDiv lcars-text" style="border-color:#0098A4;">
+        <a href="https://www.youtube.com/user/GeekologyGB" target="_blank">
+        <img src="/img/geekology_one_youtube.png" style="width:100%;margin-right:auto;margin-right:auto;"/>
+        </a>
+        <p>GeekologyOne channel on Youtube</p>
+        </div>
+        <div class="lcars-bracket bottom hollow" style="border-color:#0098A4;margin-bottom:10px"></div>
+
+        <div class="lcars-bracket top hollow"></div>
+        <div class="lcarsElementDiv lcars-text">
+        <a href="https://www.youtube.com/user/svenof9" target="_blank">
+        <img src="/img/geekology_two_youtube.png" style="width:100%;margin-right:auto;margin-right:auto;"/>
+        </a>
+        <p>SvenOf9: Geekology 2 channel on YouTube</p>
         </div>
         <div class="lcars-bracket bottom hollow"></div>
+
+
+        
     </div>
 </div>  <!-- .lcars-row -->
 @endsection
