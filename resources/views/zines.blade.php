22dc7c63f5f1b601ba763d9e81e660f488807f5f
Stefan Harvey <stefan.harvey@btopenworld.com>
2019-10-24 23:06:38 +0100

    refactor(Website): Layout for Zine's page

--- resources/views/zines.blade.php
+++ resources/views/zines.blade.php
@@ -6,8 +6,10 @@
     <div class="lcars-column lcars-u-12">
         <div class="lcars-bracket top hollow"></div>
         <div class="lcarsElementDiv lcars-text">
-        'ZINE PDFS        
+        <!-- 'ZINE PDFS -->
+        <embed src="/img/the_zine_oct_DST_Issue.pdf" type="application/pdf" width="100%" height="550px" />
         </div>
+
         <div class="lcars-bracket bottom hollow"></div>
     </div>
 </div>  <!-- .lcars-row -->
